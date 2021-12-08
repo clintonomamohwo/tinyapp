04e58facae503a07d1a9c428a677d7776aee0785
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2021-12-07 19:04:36 -0500

    URL shortening Part 2

--- express_server.js
+++ express_server.js
@@ -46,3 +46,7 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  res.redirect(longURL);
+});
